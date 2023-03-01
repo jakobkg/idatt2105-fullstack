2b38508197351ca09ac59fce86e727fd471fe044
Jakob Grønhaug <jakob@gronha.ug>
2023-03-01 13:02:05 +0100

    Legg til litt kjekk logging

--- oving04/backend/src/main/java/edu/jakobkg/calculatorbackend/services/CalculatorService.java
+++ oving04/backend/src/main/java/edu/jakobkg/calculatorbackend/services/CalculatorService.java
@@ -2,21 +2,34 @@ package edu.jakobkg.calculatorbackend.services;
 
 import org.springframework.stereotype.Service;
 
+import ch.qos.logback.classic.Logger;
+
+import org.slf4j.LoggerFactory;
 import edu.jakobkg.calculatorbackend.models.CalculationResult;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
 @Service
 public class CalculatorService {
-    public CalculationResult calculate(String expression) {
-        Expression e = new ExpressionBuilder(expression).build();
 
-        if (e.validate().isValid()) {
-            //LOGG
-            return new CalculationResult(e.evaluate(), "OK");
+    Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+    public CalculationResult calculate(String expression) {
+        Expression exp;
+
+        try {
+            exp = new ExpressionBuilder(expression).build();
+        } catch (Exception e) {
+            logger.warn("Kunne ikke konstruere Expression-objekt fra uttrykket, svarer med advarsel");
+            return new CalculationResult(0., "Ugyldig uttrykk!");
+        }
+
+        if (exp.validate().isValid()) {
+            logger.info("Uttrykket er gyldig, evaluerer det og svarer med status \"OK\"");
+            return new CalculationResult(exp.evaluate(), "OK");
         } else {
-            //LOGG
-            return new CalculationResult(0., "Invalid expression");
+            logger.warn("Uttrykket er ikke gyldig, svarer med en advarsel om dette");
+            return new CalculationResult(0., "Ugyldig uttrykk");
         }
     }
 }
